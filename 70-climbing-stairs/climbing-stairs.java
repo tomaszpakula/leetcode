6f2441eb0e89d24c685fe8197c3bc857c805afbd
tomaszpakula <92781896+tomaszpakula@users.noreply.github.com>
2024-04-12 14:36:20 +0200

    Time: 0 ms (100.00%) | Memory: 40.4 MB (25.29%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int climbStairs(int n) {
+        
+        if(n == 1 || n == 2) return n;
+        
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(1, 1);
+        map.put(2,2);
+        
+        for(int i = 3; i <= n; i++){
+            map.put(i,map.get(i-1) + map.get(i-2));
+        }
+        
+        return map.get(n);
+        
+    }
+}
