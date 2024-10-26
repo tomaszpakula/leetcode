e9efeaf313c8e07081ef72c5dc8326e9ca18940b
tomaszpakula <92781896+tomaszpakula@users.noreply.github.com>
2024-10-26 14:37:20 +0200

    Time: 13 ms (31.13%) | Memory: 49.5 MB (76.17%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,5 +1,6 @@
 class Solution {
     public int majorityElement(int[] nums) {
+        if (nums.length == 1) return nums[0];
         int max = 0;
         int maxCount = 0;
         int tempCount = 0;
