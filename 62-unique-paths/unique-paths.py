dc3fb6b9a5e5371d41fcece75cc55bb8cf1a55d2
tomaszpakula <92781896+tomaszpakula@users.noreply.github.com>
2026-02-14 16:56:25 +0100

    Time: 0 ms (100.00%) | Memory: 19.4 MB (28.61%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,14 +1,14 @@
 class Solution:
     def Paths(self, m: int, n:int, paths: list[int]) -> int:
         M, N = len(paths)-m, len(paths[0])-n
-        if paths[M][N] != -1:
-            return paths[M][N]
 
         if m == 1 or n == 1:
             paths[M][N] = 1
             return 1
+
+        if paths[M][N] == -1:
+            paths[M][N] = self.Paths(m, n-1, paths) + self.Paths(m-1, n, paths)
         
-        paths[M][N] = self.Paths(m, n-1, paths) + self.Paths(m-1, n, paths)
         return paths[M][N]
     
     def uniquePaths(self, m: int, n: int) -> int:
